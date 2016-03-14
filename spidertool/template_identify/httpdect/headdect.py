a74ed865d963a018472075eca977d9d71893877d
nanshihui <nanshihui@qq.com>
2016-03-14 08:20:05 +0800

    更新扩展参数

--- spidertool/template_identify/httpdect/headdect.py
+++ spidertool/template_identify/httpdect/headdect.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 #coding:utf-8
-import webdection 
+from spidertool.template_identify.httpdect.webdection import main
 def dect(head='',context='',ip='',port=''):
-    webdection
+#     webdection
     keywords=''
     hackinfo=''
-    w = webdection.WMap(mode=192, logger=None)
-    keywords= str(w.detect(ip+':'+str(port)))
+    if port =='443':
+        w = main.getwebinfo(ip,1)
+    else:
+        w = main.getwebinfo(ip,0)
+    keywords= str(w)
     print keywords
     return keywords,hackinfo
+
+# w = main.main('www.baidu.com',1)
+# print w
